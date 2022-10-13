93a7d47c9f996e10be636acc66cf6b87a9c54571
Rafael De La Cruz <delacruz.r22@gmail.com>
2022-10-13 15:46:32 -0400

    Added comments to files

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,12 @@
 from engine import BlackJackEngine
 
+
 def run():
+    '''Main file. When runned it will run the game as a whole. '''
+
     game = BlackJackEngine()
     game.start()
+
 
 if __name__ == "__main__":
     run()
--- deckOfCards.py
+++ deckOfCards.py
@@ -1,30 +1,63 @@
 import random
 
+
 class Deck:
+    '''Contains somethings needed to create a deck of cards'''
+
+    # --------------------------------------------------------------------------
     def __init__(self):
-        self.deck = [] 
-        self.suits = ["spades","clubs","diamonds","hearts"]
+        '''Constructor 
+        post: creates a deck of cards in order 1-13. spades -> hearts'''
+
+        self.deck = []
+        self.suits = ["spades", "clubs", "diamonds", "hearts"]
         self.createDeck()
 
+    # --------------------------------------------------------------------------
     def getDeckCount(self):
+        '''Uses the built-in function to get length of list
+        post: returns the current number of cards in the deck'''
+
         return len(self.deck)
 
+    # --------------------------------------------------------------------------
     def createDeck(self):
-        for suit in self.suits:
-            for rank in range(1,14):
-                self.deck.append((suit,rank))
+        '''Creates the deck of cards
+        pre: self.suits
+        post: populates the deck list with cards, suit and rank'''
 
-    def dealCard(self): 
+        for suit in self.suits:
+            for rank in range(1, 14):
+                self.deck.append((suit, rank))
+
+    # --------------------------------------------------------------------------
+    def dealCard(self):
+        '''Deals a card to any player 
+        post: removes the last index in the deck list'''
+
         return self.deck.pop(-1)
 
+    # --------------------------------------------------------------------------
     def shuffleDeck(self):
+        '''Shuffles the deck 
+        post: randomizes the cards in the deck'''
+
         random.shuffle(self.deck)
 
+    # --------------------------------------------------------------------------
     def resetDeck(self):
+        '''Clears the deck of cards and recreates the deck
+        post: a new deck of cards in order 1 - 13 spades -> hearts'''
+
         self.deck.clear()
         self.createDeck()
 
+
+    # --------------------------------------------------------------------------
 if __name__ == "__main__":
+    '''When file is runned the below test is runed to check class methods 
+    Needs more and proper testing'''
+
     test = Deck()
 
     # test.createDeck()
@@ -32,13 +65,13 @@ if __name__ == "__main__":
     for i in range(4):
         print(test.dealCard())
 
-    for i in range(20): 
+    for i in range(20):
         test.shuffleDeck()
-    
-    print("Deck Shuffle")
-    
+
+    print("\nDeck Shuffle")
+
     for i in range(4):
         print(test.dealCard())
 
     test.resetDeck()
-    print(len(test.getDeckCount))
+    print("\n", test.getDeckCount())
